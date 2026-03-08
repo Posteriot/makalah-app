94c302443593db52cec13ffd6e1c941dda3a9c8c
Erik Supit <erik.supit@gmail.com>
2026-03-09 00:03:08 +0700

    feat(ui-core): restyle hero with core non-chat tokens

--- src/components/marketing/hero/HeroResearchMock.tsx
+++ src/components/marketing/hero/HeroResearchMock.tsx
@@ -6,8 +6,7 @@ export function HeroResearchMock() {
     <div
       className={cn(
         "hidden md:block absolute w-full transition-all duration-300 rounded-md",
-        "dark:shadow-[-12px_12px_0px_0px_rgba(168,162,158,0.2)]",
-        "shadow-[-12px_12px_0px_0px_rgba(68,64,60,0.3)]",
+        "shadow-lg",
         "z-10 top-1/2 -translate-y-1/2 scale-[0.88] -translate-x-[60px]"
       )}
     >
