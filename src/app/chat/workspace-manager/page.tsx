1e6b4f4f47c8ec999398564e1d58069fd595b248
Erik Supit <erik.supit@gmail.com>
2026-03-11 04:24:16 +0700

    refactor: remove workspace manager route

--- src/app/chat/workspace-manager/page.tsx
+++
@@ -1,5 +0,0 @@
-import { WorkspaceManagerShell } from "@/components/chat/workspace-manager/WorkspaceManagerShell"
-
-export default function WorkspaceManagerPage() {
-  return <WorkspaceManagerShell />
-}
